25688b8a90b471f09fa4d962af120dd3ded3cee3
Rajdeep <hi@rajdeepdeb.me>
2019-07-26 22:34:14 +0530

    add filter based on status of ride

--- thrive-app/src/components/pages/RideListPage.js
+++ thrive-app/src/components/pages/RideListPage.js
@@ -10,12 +10,24 @@ class RideListPage extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			rides: []
+			rides: [],
+			pending : true,
+			active : true,
+			done : true
 		};
 	}
 
 	componentDidMount() {
 		this.refreshList();
+	}
+
+	onChange(ev) {
+		const target = ev.target;
+		const key = target.id;
+		const value = target.type === 'checkbox' ? target.checked : target.value;
+		this.setState({
+			[key]: value
+		});
 	}
 
 	refreshList() {
@@ -48,6 +60,7 @@ class RideListPage extends Component {
 
 	render() {
 		let ridesHTML = this.state.rides.map((ride, index) => {
+			if(this.state[ride.status] === false) return undefined;
 			return <RideComponent key={ride.id} ride={ride} />
 		});
 		if(ridesHTML.length===0) {
@@ -63,9 +76,26 @@ class RideListPage extends Component {
 					<MDBCol md="6" lg="12" className="mb-5 mt-1">
 					{
 					user.type==="driver" ?
-					<strong>You can accept any pending rides</strong> :
-						<input type="button" value="Request a ride" className="rounded border p-2" onClick={this.requestRide.bind(this)} />
+					<strong>
+					You may accept any pending rides
+					<input type="button" value="Refresh List" className="rounded border p-2 ml-2" onClick={this.refreshList.bind(this)} />
+					</strong>
+					:
+					<strong>
+					You may request a ride
+						<input type="button" value="Request Ride" className="rounded border p-2 ml-2 mr-2" onClick={this.requestRide.bind(this)} />
+						 OR refresh to check updated status
+						<input type="button" value="Refresh List" className="rounded border p-2 ml-2" onClick={this.refreshList.bind(this)} />
+					</strong>
 					}
+					</MDBCol>
+					<MDBCol>
+						<label htmlFor="active" className="ml-0">Active</label>
+						<input onChange={(ev) => this.onChange(ev)} type="checkbox" checked={this.state.active} id="active" className="p-3 m-3 mr-5" />
+						<label htmlFor="pending" className="ml-0">Pending</label>
+						<input onChange={(ev) => this.onChange(ev)} type="checkbox" checked={this.state.pending} id="pending" className="p-3 m-3 mr-5" />
+						<label htmlFor="done" className="ml-0">Done</label>
+						<input onChange={(ev) => this.onChange(ev)} type="checkbox" checked={this.state.done} id="done" className="p-3 m-3 mr-5" />
 					</MDBCol>
 					<MDBCol md="6" lg="12">
 						<section className="text-center pb-3">
